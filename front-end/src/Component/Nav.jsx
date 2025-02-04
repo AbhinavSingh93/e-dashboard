3df6533c76e844faf0015974edbfded27ed0b2e8
*AbhinavSingh93* <*abhinavsingh18813@gmail.com*>
2025-02-04 08:32:29 +0530

    added user name to nav and hiding of nav using auth

--- front-end/src/Component/Nav.jsx
+++ front-end/src/Component/Nav.jsx
@@ -1,28 +1,28 @@
 import React, { useEffect } from 'react'
-import{Link,useNavigate} from 'react-router-dom'
-const Nav=()=>{
-  const auth=localStorage.getItem('user');
-  const navigate=useNavigate();
-  const logout=()=>{
+import { Link, useNavigate } from 'react-router-dom'
+const Nav = () => {
+  const auth = localStorage.getItem('user');
+  const navigate = useNavigate();
+  const logout = () => {
     localStorage.clear();
     navigate('/signup');
   }
   return (
     <div>
-      <ul className='nav-ul'>
+      <img className='logo' src="https://i.ibb.co/TD1YK7Qw/DALL-E-2025-02-04-08-18-56-A-modern-minimalist-logo-for-an-e-commerce-dashboard-The-logo-features-a.png" alt="DALL-E-2025-02-04-08-18-56-A-modern-minimalist-logo-for-an-e-commerce-dashboard-The-logo-features-a" />
+      {auth ? <ul className='nav-ul'>
         <li><Link to="/">Products</Link></li>
         <li><Link to="/add">Add Product</Link></li>
         <li><Link to="/update">Update Product</Link></li>
         <li><Link to="/profile">Profile</Link></li>
-        {
-          auth ? <li><Link onClick={logout} to="/signup">Logout</Link></li>
-          :
-          <>
+        <li><Link onClick={logout} to="/signup">Logout ({JSON.parse(auth).name})</Link></li>
+      </ul>
+        :
+        <ul className='nav-ul nav-right'>
           <li><Link to="/signup">SignUp</Link></li>
           <li><Link to="/login">Login</Link></li>
-          </>
-        }
-      </ul>
+        </ul>
+      }
     </div>
   )
 }
